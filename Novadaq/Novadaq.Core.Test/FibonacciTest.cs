dc2c4ed0b1ad9bddbaf912a808a36a568fde0078
Lam Le <lam281990@gmail.com>
2017-09-27 12:31:06 -0700

    Finished implementing Fibonacci finder

--- Novadaq/Novadaq.Core.Test/FibonacciTest.cs
+++ Novadaq/Novadaq.Core.Test/FibonacciTest.cs
@@ -14,5 +14,32 @@ namespace Novadaq.Core.Test
                 FibonacciFinder.GetAt(0);
             });
         }
+
+        private static TheoryData<long, long> GetFibonacciSequence()
+        {
+            return new TheoryData<long, long>()
+            {
+                {1, 1},
+                {2, 1},
+                {3, 2},
+                {4, 3},
+                {5, 5},
+                {6, 8},
+                {50, 12_586_269_025}
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFibonacciSequence))]
+        public void PhoneValidatorTest(long n, long expectedValue)
+        {
+            //Assign
+            var input = n;
+            var expected = expectedValue;
+            //Act
+            var resultTest = FibonacciFinder.GetAt(input);
+            //Assert
+            Assert.Equal(resultTest, expected);
+        }
     }
 }
--- Novadaq/Novadaq.Core/FibonacciFinder.cs
+++ Novadaq/Novadaq.Core/FibonacciFinder.cs
@@ -4,17 +4,17 @@ namespace Novadaq.Core
 {
     public static class FibonacciFinder
     {
-        public static int GetAt(int n)
+        public static long GetAt(long n)
         {
             if (n < 1)
             {
                 throw new ArgumentException("The position in the sequence should be greater than or equal 1");
             }
-            if (n == 1)
+            if (n < 3)
             {
                 return 1;
             }
-            return n + GetAt(n - 1);
+            return GetAt(n - 1) + GetAt(n - 2);
         }
     }
 }
